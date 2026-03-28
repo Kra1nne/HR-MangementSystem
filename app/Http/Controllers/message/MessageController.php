a46266b7e0d5750bea58c6b55c9ed90936a46c03
Kra1nne <cabarrubias2003@gmail.com>
2026-03-28 17:05:46 +0800

    refactor: Uncomment the broadcast Mail

--- app/Http/Controllers/message/MessageController.php
+++ app/Http/Controllers/message/MessageController.php
@@ -34,31 +34,31 @@ class MessageController extends Controller
         }
     }
     public function broadcastMessage(Request $request){
-        // $emails = Employee::leftjoin('users', 'users.person_id', 'employees.person_id')
-        //     ->whereNull('employees.deleted_at')
-        //     ->pluck('users.email') 
-        //     ->toArray();
+        $emails = Employee::leftjoin('users', 'users.person_id', 'employees.person_id')
+            ->whereNull('employees.deleted_at')
+            ->pluck('users.email') 
+            ->toArray();
 
-        //  $data = [
-        //     'name' => 'Employee',
-        //     'title' => $request->messageTitle,
-        //     'description' => $request->messageContent
-        // ];
+         $data = [
+            'name' => 'Employee',
+            'title' => $request->messageTitle,
+            'description' => $request->messageContent
+        ];
         
-        // try {
-        //     Mail::bcc($emails)->send(new MessageMail($data));
+        try {
+            Mail::bcc($emails)->send(new MessageMail($data));
 
-        //     return response()->json([
-        //         'Error' => 0,
-        //         'Message' => 'Message successfully sent'
-        //     ]);
-        // } catch (\Exception $e) {
-        //     return response()->json([
-        //         'Error' => 1,
-        //         'Message' => 'Failed to send message',
-        //         'Details' => $e->getMessage()
-        //     ]);
-        // }
+            return response()->json([
+                'Error' => 0,
+                'Message' => 'Message successfully sent'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'Error' => 1,
+                'Message' => 'Failed to send message',
+                'Details' => $e->getMessage()
+            ]);
+        }
         return response()->json(['Error' => 0, 'Message' => 'Message successfully sent']);
     }
     public function broadcastMessageDepartment(Request $request)
